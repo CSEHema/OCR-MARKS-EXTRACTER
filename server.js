82758806cefc8fa909b576a09d2dec8fd55074aa
CSEHema <hemasiri1405@gmail.com>
2024-09-03 19:18:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
 
 // Create connection to MySQL database
 const db = mysql.createPool({
-    connectionLimit:10,
+    connectionLimit:5,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,  // Your MySQL password
